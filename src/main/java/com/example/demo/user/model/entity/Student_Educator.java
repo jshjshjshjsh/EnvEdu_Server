54fe836f70b43980f35eef95a2d81cc88bc7145c
201724554 <pay7845@naver.com>
2023-04-10 20:53:42 +0900

    #94-add/id 추가

--- src/main/java/com/example/demo/user/model/entity/Student_Educator.java
+++ src/main/java/com/example/demo/user/model/entity/Student_Educator.java
@@ -1,11 +1,13 @@
 package com.example.demo.user.model.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 public class Student_Educator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     @ManyToOne
     @JoinColumn(name = "studentId")
     private Student student;
