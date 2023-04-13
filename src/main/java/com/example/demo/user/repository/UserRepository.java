228f1c44913ace8a70a8c98bb65244b66d34063c
201724554 <pay7845@naver.com>
2023-04-13 21:42:54 +0900

    #94-feat/메소드 파라미터 수정(IsActive -> State)

--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -1,16 +1,15 @@
 package com.example.demo.user.repository;
 
 import com.example.demo.user.model.entity.User;
-import com.example.demo.user.model.enumerate.IsActive;
+import com.example.demo.user.model.enumerate.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean existsByUsername(String username);
-    boolean existsByEmailAndIsActive(String email, IsActive isActive);
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsernameAndState(String username, State state);
+    boolean existsByEmailAndState(String email, State state);
     Optional<User> findByUsername(String username);
     Optional<User> findByUsernameAndEmail(String username, String email);
-    Optional<User> findByUsernameAndIsActive(String username, IsActive isActive);
+    Optional<User> findByUsernameAndState(String username, State state);
 }
