903ac45ab5d6acc607214b4ff6ee41834b34d095
201724554 <pay7845@naver.com>
2023-04-13 21:53:43 +0900

    #94-feat/이메일 인증 및 회원가입 처리 메소드 구현

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -2,58 +2,94 @@ package com.example.demo.user.service;
 
 import com.example.demo.device.model.UserDevice;
 import com.example.demo.device.repository.UserDeviceRepository;
-import com.example.demo.mail.service.MailService;
-import com.example.demo.redis.entity.RegisterAuthNum;
-import com.example.demo.redis.repo.RegisterAuthNumRepository;
+import com.example.demo.exceptions.CustomMailException;
+import com.example.demo.exceptions.DuplicateAttributeException;
+import com.example.demo.redis.entity.AuthNum;
+import com.example.demo.redis.repo.AuthNumRepository;
 import com.example.demo.token.repository.RefreshTokenRepository;
-import com.example.demo.user.model.entity.Educator;
+import com.example.demo.user.dto.EmailDTO;
+import com.example.demo.user.dto.RegisterDTO;
 import com.example.demo.user.model.entity.Student;
 import com.example.demo.user.model.entity.User;
-import com.example.demo.user.model.enumerate.IsActive;
-import com.example.demo.user.model.enumerate.Role;
+import com.example.demo.user.model.enumerate.State;
 import com.example.demo.user.repository.UserRepository;
+import com.example.demo.user.util.Utils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final UserRepository userRepository;
-    private final RegisterAuthNumRepository registerAuthNumRepository;
-    private final MailService mailService;
+    private final AuthNumRepository authNumRepository;
     private final UserDeviceRepository userDeviceRepository;
     private final RefreshTokenRepository refreshTokenRepository;
 
     /**
      register service
      */
+    @Transactional
+    public void addUser(RegisterDTO registerDTO) {
+        User user = registerDTO.getRole().generateUserByRole(registerDTO, bCryptPasswordEncoder);
+        if(userRepository.existsByUsernameAndState(user.getUsername(), State.ACTIVE)) {
+            throw new DuplicateAttributeException("아이디");
+        }
+        userRepository.save(user);
+    }
+
+    @Transactional(rollbackFor = {MessagingException.class, MailException.class})
+    public void sendAuthNum(EmailDTO emailDTO) throws CustomMailException, DuplicateAttributeException
+    {
+        if(userRepository.existsByEmailAndState(emailDTO.getEmail(), State.ACTIVE)) {
+            throw new DuplicateAttributeException("이메일");
+        }
+        String authNum = Utils.generateAuthNum();
+        //mailService.sendAuthMail(emailDTO, authNum);
+        authNumRepository.save(AuthNum.of(emailDTO.getEmail(), authNum));
+    }
+
+    @Transactional
+    public void checkAuthNum(String email, String authNum) {
+        AuthNum auth = authNumRepository.findById(email).orElseThrow(()->new IllegalArgumentException("인증정보가 틀렸습니다"));
+        authNumRepository.deleteById(email);
+        if(!auth.getAuthNum().equals(authNum)) {
+            throw new IllegalArgumentException("인증정보가 틀렸습니다");
+        }
+    }
+
+
     @Transactional(readOnly = true)
     public boolean checkDuplicateUsernameAndEmail(String username, String email)
     {
-        return userRepository.existsByUsername(username) || userRepository.existsByEmailAndIsActive(email,IsActive.YES);
+        //return userRepository.existsByUsername(username) || userRepository.existsByEmailAndState(email, State.ACTIVE);
+        return true;
     }
     @Transactional
-    public void addUser(User user, RegisterAuthNum registerAuthNum)
+    public void addUser(User user, AuthNum authNum)
     {
         userRepository.save(user);
         //registerAuthNumRepository.save(registerAuthNum);
-        //mailService.sendAuthMail(user.getEmail(),registerAuthNum.getRegisterAuthNum());
+        //mailService.sendAuthMail(user.getEmail(),registerAuthNum.getAuthNum());
     }
 
     @Transactional
     public void addStudent(String username, String studentUsername)
     {
-        Student student = (Student) userRepository.findByUsernameAndIsActive(studentUsername, IsActive.YES).orElseThrow(()->{throw new IllegalArgumentException();});
-        student.setEducator((Educator) userRepository.findByUsernameAndIsActive(username, IsActive.YES).orElseThrow(()->{throw new IllegalArgumentException();}));
+        Student student = (Student) userRepository.findByUsernameAndState(studentUsername, State.ACTIVE).orElseThrow(()->{throw new IllegalArgumentException();});
+        //student.setEducator((Educator) userRepository.findByUsernameAndIsActive(username, State.ACTIVE).orElseThrow(()->{throw new IllegalArgumentException();}));
     }
     @Transactional
     public void confirmAuthentication(String username, String email, String authNum)
     {
-        RegisterAuthNum registerAuthNum = registerAuthNumRepository.findById(email).orElseThrow(()->{throw new IllegalArgumentException();});
-        if(!registerAuthNum.getRegisterAuthNum().equals(authNum))
+        AuthNum registerAuthNum = authNumRepository.findById(email).orElseThrow(()->{throw new IllegalArgumentException();});
+        if(!registerAuthNum.getAuthNum().equals(authNum))
         {
             throw new IllegalArgumentException();
         }
@@ -63,15 +99,15 @@ public class UserService {
         {
             throw new IllegalArgumentException();
         }
-        optUser.get().setIsActive(IsActive.YES);
+        optUser.get().setState(State.ACTIVE);
 
-        registerAuthNumRepository.deleteById(email);
+        authNumRepository.deleteById(email);
     }
     @Transactional
-    public void resendAuthNum(RegisterAuthNum registerAuthNum)
+    public void resendAuthNum(AuthNum authNum)
     {
-        mailService.sendAuthMail(registerAuthNum.getEmail(),registerAuthNum.getRegisterAuthNum());
-        registerAuthNumRepository.save(registerAuthNum);
+        //mailService.sendAuthMail(registerAuthNum.getEmail(),registerAuthNum.getAuthNum());
+        authNumRepository.save(authNum);
     }
 
     /**
@@ -80,13 +116,13 @@ public class UserService {
     @Transactional(readOnly = true)
     public User getUser(String username)
     {
-        return userRepository.findByUsernameAndIsActive(username, IsActive.YES).orElseThrow(()->{throw new IllegalArgumentException();});
+        return userRepository.findByUsernameAndState(username, State.ACTIVE).orElseThrow(()->{throw new IllegalArgumentException();});
     }
     @Transactional
     public void addMAC(String username, String MAC)
     {
         User user = userRepository.findByUsername(username).orElseThrow(IllegalArgumentException::new);
-        UserDevice userDevice = userDeviceRepository.findByUserDeviceMAC(MAC).orElseThrow(IllegalArgumentException::new);
+        UserDevice userDevice = userDeviceRepository.findByMac(MAC).orElseThrow(IllegalArgumentException::new);
         userDevice.setUser(user);
     }
 
@@ -108,7 +144,7 @@ public class UserService {
         User user = userRepository.findByUsername(username).orElse(null);
         if(user != null)
         {
-            user.setRole(Role.ROLE_ADMIN.toString());
+            //user.setRole(Role.ROLE_ADMIN.toString());
         }
     }
 }
