3a20aa43dcba26abb67adc45d72668f4515de7a8
201724554 <pay7845@naver.com>
2023-04-14 03:52:08 +0900

    #94-feat/loginAdmin 메소드 추가

--- src/main/java/com/example/demo/admin/service/AdminService.java
+++ src/main/java/com/example/demo/admin/service/AdminService.java
@@ -1,5 +1,8 @@
 package com.example.demo.admin.service;
 
+import com.example.demo.admin.DTO.AdminLoginDTO;
+import com.example.demo.admin.cipher.AdminCipher;
+import com.example.demo.admin.model.Admin;
 import com.example.demo.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,5 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+    private final AdminCipher adminCipher;
+
     private final AdminRepository adminRepository;
+
+    public void loginAdmin(AdminLoginDTO adminLoginDTO) {
+        Admin admin = adminRepository.findByUsername(adminLoginDTO.getUsername()).orElseThrow(()->new IllegalArgumentException("로그인 정보가 일치하지 않습니다"));
+        if(!adminCipher.encrypt(adminLoginDTO.getPassword()).equals(admin.getPassword())) {
+            throw new IllegalArgumentException("로그인 정보가 일치하지 않습니다");
+        }
+    }
 }
