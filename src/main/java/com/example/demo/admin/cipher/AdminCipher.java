53e81cd6ace3edce207db7b44004a98c81025b25
201724554 <pay7845@naver.com>
2023-04-19 02:50:17 +0900

    #94-fix/@Value 어노테이션에 닫는 대괄호 추가

--- src/main/java/com/example/demo/admin/cipher/AdminCipher.java
+++ src/main/java/com/example/demo/admin/cipher/AdminCipher.java
@@ -4,22 +4,16 @@ import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @Component
 public class AdminCipher {
     @Value("${spring.cipher.key}")
     private String secretKey;
 
-    @Value("${spring.cipher.transformation")
+    @Value("${spring.cipher.transformation}")
     private String transformation;
 
     public String encrypt(String target) {
@@ -34,7 +28,7 @@ public class AdminCipher {
 
             cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, transformation));
             return new String(Hex.encodeHex(cipher.doFinal(target.getBytes(StandardCharsets.UTF_8)))).toUpperCase();
-        } catch(Exception e) {
+        } catch (Exception e) {
             return target;
         }
     }
