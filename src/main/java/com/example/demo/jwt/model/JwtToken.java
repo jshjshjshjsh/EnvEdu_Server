edd8314be8e00036d9f7ef06f4fa04d7fad713ab
201724554 <pay7845@naver.com>
2023-04-20 02:04:20 +0900

    #94-feat/JwtAccessToken과 JwtRefreshToken 클래스가 상속받을 JwtToken 클래스 추가

---
+++ src/main/java/com/example/demo/jwt/model/JwtToken.java
@@ -0,0 +1,46 @@
+package com.example.demo.jwt.model;
+
+import com.example.demo.admin.model.Admin;
+import com.example.demo.user.model.entity.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class JwtToken {
+    private final String subject;
+
+    private final Date expiresAt;
+
+    private final Map<String, String> claims;
+
+    protected JwtToken(String subject, Date expiresAt, User user) {
+        this.subject = subject;
+        this.expiresAt = expiresAt;
+        Map<String, String> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        claims.put("role", user.getRole().toString());
+        this.claims = claims;
+    }
+
+    protected JwtToken(String subject, Date expiresAt, Admin admin) {
+        this.subject = subject;
+        this.expiresAt = expiresAt;
+        Map<String, String> claims = new HashMap<>();
+        claims.put("username", admin.getUsername());
+        claims.put("role", "ROLE_ADMIN");
+        this.claims = claims;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public Date getExpiresAt() {
+        return this.expiresAt;
+    }
+
+    public Map<String, String> getClaims() {
+        return this.claims;
+    }
+}
