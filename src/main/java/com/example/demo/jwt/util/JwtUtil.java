a2a78eaf0ccd247da83da343f98c74f12c4f96d3
201724554 <pay7845@naver.com>
2023-04-20 02:06:01 +0900

    #94-feat/jwtToken을 String으로 변환, 검증 등을 위한 component 클래스 추가

---
+++ src/main/java/com/example/demo/jwt/util/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.example.demo.jwt.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.jwt.model.JwtToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class JwtUtil {
+    public static String jwtKey;
+
+    @Value("${spring.jwt.key}")
+    private String key;
+
+    public static String convertJwtToString(JwtToken jwtToken) {
+        return "Bearer " +
+                JWT.create()
+                        .withSubject(jwtToken.getSubject())
+                        .withExpiresAt(jwtToken.getExpiresAt())
+                        .withClaim("user_info", jwtToken.getClaims())
+                        .sign(Algorithm.HMAC512(jwtKey));
+    }
+
+    @PostConstruct
+    private void init() {
+        this.jwtKey = key;
+    }
+}
