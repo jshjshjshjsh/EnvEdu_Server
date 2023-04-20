3d0576e1a97c1c0faa0f7425f08034e6a1e0b455
201724554 <pay7845@naver.com>
2023-04-21 03:18:43 +0900

    #94-feat/id Long으로 변경 및 메소드 명 변경

--- src/main/java/com/example/demo/device/repository/UserDeviceRepository.java
+++ src/main/java/com/example/demo/device/repository/UserDeviceRepository.java
@@ -3,15 +3,13 @@ package com.example.demo.device.repository;
 import com.example.demo.device.model.UserDevice;
 import com.example.demo.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-public interface UserDeviceRepository extends JpaRepository<UserDevice, Integer> {
-    Optional<UserDevice> findByUserDeviceMAC(String userDeviceMAC);
-    boolean existsByUserDeviceMAC(String userDeviceMAC);
+public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
+    Optional<UserDevice> findByMac(String userDeviceMAC);
+    boolean existsByMac(String userDeviceMAC);
     List<UserDevice> findAllByUser(User user);
 
     List<UserDevice> findAllByUserIn(List<User> list);
