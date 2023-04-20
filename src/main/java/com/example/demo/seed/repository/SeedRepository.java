0b60591f699e4b8d56b91a6b8cc7bdb7059fed89
201724554 <pay7845@naver.com>
2023-04-21 03:15:56 +0900

    #94-feat/id Long으로 변경 및 메소드 명 변경

--- src/main/java/com/example/demo/seed/repository/SeedRepository.java
+++ src/main/java/com/example/demo/seed/repository/SeedRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface SeedRepository extends JpaRepository<Seed, Integer> {
-    List<Seed> findAllByDateBetweenAndMacIn(LocalDateTime start, LocalDateTime end, List<String> MAC);
-    List<Seed> findAllByDateBetween(LocalDateTime start, LocalDateTime end);
+public interface SeedRepository extends JpaRepository<Seed, Long> {
+    List<Seed> findAllByMeasuredDateBetweenAndMacIn(LocalDateTime start, LocalDateTime end, List<String> MAC);
+    List<Seed> findAllByMeasuredDateBetween(LocalDateTime start, LocalDateTime end);
 }
