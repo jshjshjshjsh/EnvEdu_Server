be697f4bfe0c5242d9ca76d22cadcc14e7fbf963
201724554 <pay7845@naver.com>
2023-04-22 22:29:28 +0900

    #94-feat/PrincipalDetails를 인자로 받는 생성자 추가

--- src/main/java/com/example/demo/jwt/model/JwtToken.java
+++ src/main/java/com/example/demo/jwt/model/JwtToken.java
@@ -1,6 +1,7 @@
 package com.example.demo.jwt.model;
 
 import com.example.demo.admin.model.Admin;
+import com.example.demo.security.principal.PrincipalDetails;
 import com.example.demo.user.model.entity.User;
 
 import java.util.Date;
@@ -20,6 +21,15 @@ public abstract class JwtToken {
         Map<String, String> claims = new HashMap<>();
         claims.put("username", user.getUsername());
         claims.put("role", user.getRole().toString());
+        this.claims = claims;
+    }
+
+    protected JwtToken(String subject, Date expiresAt, PrincipalDetails principalDetails) {
+        this.subject = subject;
+        this.expiresAt = expiresAt;
+        Map<String, String> claims = new HashMap<>();
+        claims.put("username", principalDetails.getUsername());
+        claims.put("role", principalDetails.getRole());
         this.claims = claims;
     }
 
