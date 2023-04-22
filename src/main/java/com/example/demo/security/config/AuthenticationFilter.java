7ac5a6a0f0ce5f476051cf8347a4fa0470936109
201724554 <pay7845@naver.com>
2023-04-22 22:28:27 +0900

    #94-feat/refreshTokenRepo 제거 및 refreshToken 쿠키로 반환

--- src/main/java/com/example/demo/security/config/AuthenticationFilter.java
+++ src/main/java/com/example/demo/security/config/AuthenticationFilter.java
@@ -1,14 +1,15 @@
 package com.example.demo.security.config;
 
-import com.example.demo.security.jwt.JwtUtil;
-import com.example.demo.security.jwt.Properties;
+import com.example.demo.cookie.util.CookieUtil;
+import com.example.demo.jwt.model.JwtAccessToken;
+import com.example.demo.jwt.model.JwtRefreshToken;
+import com.example.demo.jwt.util.JwtUtil;
 import com.example.demo.security.principal.PrincipalDetails;
 import com.example.demo.user.dto.LoginDTO;
-import com.example.demo.token.model.RefreshToken;
-import com.example.demo.token.repository.RefreshTokenRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,37 +19,33 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RequiredArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
-    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         ObjectMapper om = new ObjectMapper();
         LoginDTO loginDTO = null;
-        try
-        {
+
+        try {
             loginDTO = om.readValue(request.getInputStream(), LoginDTO.class);
         }
         catch (Exception e) {
             response.setStatus(HttpStatus.BAD_REQUEST.value());
         }
-        if(loginDTO != null)
-        {
+
+        if(loginDTO != null) {
             try {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         loginDTO.getUsername(), loginDTO.getPassword());
 
                 return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
             }
-            catch (BadCredentialsException | UsernameNotFoundException e)
-            {
+            catch (BadCredentialsException | UsernameNotFoundException e) {
                 response.setStatus(HttpStatus.UNAUTHORIZED.value());
             }
         }
@@ -56,18 +53,16 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
         PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
 
-        RefreshToken refreshToken = RefreshToken.builder()
-                .username(principalDetails.getUsername())
-                .refreshToken(JwtUtil.makeAccessJwt(principalDetails.getUsername(), Properties.REFRESH))
-                .build();
+        JwtRefreshToken jwtRefreshToken = JwtRefreshToken.generateJwtRefreshToken(principalDetails);
+        JwtAccessToken jwtAccessToken = JwtAccessToken.generateJwtAccessToken(principalDetails);
 
-        refreshTokenRepository.findByUsername(principalDetails.getUsername()).ifPresent(storedToken -> refreshTokenRepository.deleteByUsername(principalDetails.getUsername()));
+        String jwtAccessTokenString = JwtUtil.convertJwtToString(jwtAccessToken);
+        ResponseCookie cookie = CookieUtil.generateCookieForRefreshToken(jwtRefreshToken);
 
-        refreshTokenRepository.save(refreshToken);
-        response.setHeader(Properties.HEADER_STRING, JwtUtil.makeAccessJwt(principalDetails.getUsername(), Properties.ACCESS));
-        response.setHeader(Properties.REFRESH, refreshToken.getRefreshToken());
+        response.setHeader("Set-Cookie", cookie.toString());
+        response.setHeader(JwtUtil.headerString, JwtUtil.tokenType + jwtAccessTokenString);
     }
 }
