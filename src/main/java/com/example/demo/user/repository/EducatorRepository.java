1afb7f4147680933bdbd57d33a4c987581b60234
201724554 <pay7845@naver.com>
2023-04-28 04:53:56 +0900

    #94-feat/메소드 추가

--- src/main/java/com/example/demo/user/repository/EducatorRepository.java
+++ src/main/java/com/example/demo/user/repository/EducatorRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.user.repository;
 import com.example.demo.user.model.entity.Educator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EducatorRepository extends JpaRepository<Educator, Long> {
+    Optional<Educator> findByUsername(String username);
 }
