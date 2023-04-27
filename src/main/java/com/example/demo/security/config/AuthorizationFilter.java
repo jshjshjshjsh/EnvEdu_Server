a5b64b950dcf5fc17dc4a03b5628a4ac55e3f917
201724554 <pay7845@naver.com>
2023-04-28 04:39:49 +0900

    #94-feat/쿠키로 전달되는 refresh token만 검증하는 것으로 코드 간소화

--- src/main/java/com/example/demo/security/config/AuthorizationFilter.java
+++ src/main/java/com/example/demo/security/config/AuthorizationFilter.java
@@ -2,17 +2,15 @@ package com.example.demo.security.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.example.demo.security.jwt.JwtUtil;
-import com.example.demo.security.jwt.Properties;
+import com.auth0.jwt.interfaces.Claim;
+import com.example.demo.cookie.util.CookieUtil;
+import com.example.demo.exceptions.NoJwtTokenContainedException;
+import com.example.demo.jwt.model.JwtRefreshToken;
+import com.example.demo.jwt.util.JwtUtil;
 import com.example.demo.security.principal.PrincipalDetails;
-import com.example.demo.token.model.RefreshToken;
-import com.example.demo.token.repository.RefreshTokenRepository;
-import com.example.demo.user.model.entity.User;
-import com.example.demo.user.model.enumerate.State;
-import com.example.demo.user.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,98 +20,44 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
-    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final UserRepository userRepository;
-    public AuthorizationFilter(AuthenticationManager authenticationManager, RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
+    public AuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.userRepository = userRepository;
     }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String accessToken = request.getHeader(Properties.HEADER_STRING);
-        if(accessToken == null || !accessToken.startsWith(Properties.PREFIX))
-        {
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies == null) {
             chain.doFilter(request, response);
             return;
         }
 
-        try
-        {
-            String username = JWT.require(Algorithm.HMAC512(Properties.KEY)).build().verify(accessToken.replace(Properties.PREFIX, ""))
-                    .getClaim(Properties.CLAIM_USERNAME).asString();
+        try {
+            Map<String, Object> user_info = JwtUtil.getJwtRefreshTokenFromCookieAndParse(cookies).get(JwtUtil.claimName).asMap();
 
-            User user = userRepository.findByUsernameAndState(username, State.ACTIVE).orElseThrow(()-> new IllegalArgumentException("해당 유저가 존재하지 않습니다"));
-
-            if(user == null)
-            {
-                throw new IllegalArgumentException("유효하지 않은 토큰입니다");
-            }
-
-            PrincipalDetails principalDetails = new PrincipalDetails(user);
+            PrincipalDetails principalDetails = new PrincipalDetails(user_info);
             Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails,
                     null,
                     principalDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            chain.doFilter(request,response);
-        }
-        catch (TokenExpiredException e)
-        {
-            log.warn("토큰이 만료되었습니다");
-
-            String receivedRefreshToken = request.getHeader(Properties.REFRESH);
-
-            if(receivedRefreshToken == null)
-            {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                return;
-            }
-
-            RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(receivedRefreshToken).orElse(null);
-
-            if(refreshToken == null)
-            {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                return;
-            }
-
-            if(JWT.decode(refreshToken.getRefreshToken().replace(Properties.PREFIX,"")).getExpiresAt().before(new Date()))
-            {
-                refreshTokenRepository.deleteByRefreshToken(refreshToken.getRefreshToken());
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                return;
-            }
-
-            User user = userRepository.findByUsernameAndState(JwtUtil.getClaim(request,Properties.CLAIM_USERNAME), State.ACTIVE).orElseThrow(IllegalArgumentException::new);
-
-            //RefreshToken storedToken = refreshTokenRepository.findByUsername(user.getUsername()).orElseThrow(IllegalArgumentException::new);
-
-            //String newRefreshToken = JwtUtil.makeAccessJwt(user.getUsername(), Properties.REFRESH);
-            //storedToken.setRefreshToken(newRefreshToken);
-
-            PrincipalDetails principalDetails = new PrincipalDetails(user);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails,
-                    null,
-                    principalDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            response.setHeader(Properties.HEADER_STRING, JwtUtil.makeAccessJwt(user.getUsername(), Properties.ACCESS));
-            //response.setHeader(Properties.REFRESH, newRefreshToken);
-
-            chain.doFilter(request,response);
-        }
-        catch(IllegalArgumentException e)
-        {
-            log.warn(e.getMessage());
+            chain.doFilter(request, response);
+        } catch (JWTVerificationException e) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setHeader("Set-Cookie", CookieUtil.generateLogoutCookie().toString());
+        } catch (NoJwtTokenContainedException e) {
+            chain.doFilter(request, response);
         }
     }
 }
