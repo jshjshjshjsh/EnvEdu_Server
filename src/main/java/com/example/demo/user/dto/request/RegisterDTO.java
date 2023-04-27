aa98055d4b92f46fdbe56295f7bd472d1956493b
201724554 <pay7845@naver.com>
2023-04-28 04:34:04 +0900

    #94-mv/패키지 변경

--- src/main/java/com/example/demo/user/dto/request/RegisterDTO.java
+++ src/main/java/com/example/demo/user/dto/request/RegisterDTO.java
@@ -1,4 +1,4 @@
-package com.example.demo.user.dto;
+package com.example.demo.user.dto.request;
 
 import com.example.demo.user.model.enumerate.Gender;
 import com.example.demo.user.model.enumerate.Role;
