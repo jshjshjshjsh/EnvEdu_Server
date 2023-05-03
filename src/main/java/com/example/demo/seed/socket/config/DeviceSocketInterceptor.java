9e1d039c54b9823195786d638877041cbe5118b9
201724554 <pay7845@naver.com>
2023-05-03 18:38:43 +0900

    #108-feat/handshake request header에 등록된 mac 주소가 있을 때만 허용하도록 구현

--- src/main/java/com/example/demo/seed/socket/config/DeviceSocketInterceptor.java
+++ src/main/java/com/example/demo/seed/socket/config/DeviceSocketInterceptor.java
@@ -1,16 +1,35 @@
 package com.example.demo.seed.socket.config;
 
+import com.example.demo.device.repository.UserDeviceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
+import java.util.Objects;
 
+@RequiredArgsConstructor
+@Slf4j
 public class DeviceSocketInterceptor implements HandshakeInterceptor {
+    private final UserDeviceRepository userDeviceRepository;
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        return false;
+        try {
+            String MAC = Objects.requireNonNull(request.getHeaders().get("mac")).toString();
+            boolean exists =  userDeviceRepository.existsByMac(MAC);
+            if(exists) {
+                log.info("connected from " + MAC);
+                return true;
+            }
+            log.warn("device connection attempt from disallowed address " + MAC);
+            return false;
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 
     @Override
