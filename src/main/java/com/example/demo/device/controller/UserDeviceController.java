b32e690ef38d8394bf5684f27b69d35eef63adf1
201724554 <pay7845@naver.com>
2023-05-07 23:30:52 +0900

    #122-refactor/사용하지 않는 api 제거

--- src/main/java/com/example/demo/device/controller/UserDeviceController.java
+++ src/main/java/com/example/demo/device/controller/UserDeviceController.java
@@ -1,31 +1,22 @@
 package com.example.demo.device.controller;
 
-import com.example.demo.DTO.AddMACDTO;
-import com.example.demo.DTO.MacListDTO;
-import com.example.demo.DTO.ResponseDTO;
+import com.example.demo.device.dto.request.AddMACDTO;
 import com.example.demo.device.dto.request.DeviceUpdateDTO;
 import com.example.demo.device.service.UserDeviceService;
-import com.example.demo.jwt.model.JwtRefreshToken;
 import com.example.demo.jwt.util.JwtUtil;
-import com.example.demo.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class UserDeviceController {
     private final UserDeviceService userDeviceService;
-    private final UserService userService;
 
     @PostMapping("/admin/device")
     private ResponseEntity<?> addDevice(@RequestBody AddMACDTO addMACDTO) {
@@ -48,13 +39,6 @@ public class UserDeviceController {
     private ResponseEntity<?> deleteDevice(@PathVariable String mac) {
         userDeviceService.deleteDevice(mac);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PostMapping("/user/device")
-    private ResponseDTO<Object> registerDevice(@Valid @RequestBody AddMACDTO addMACDTO)
-    {
-        //userDeviceService.registerDevice(addMACDTO.getUsername(), addMACDTO.getMAC());
-        return new ResponseDTO<>(HttpStatus.OK.value(), null);
     }
 
     @GetMapping("/user/device")
