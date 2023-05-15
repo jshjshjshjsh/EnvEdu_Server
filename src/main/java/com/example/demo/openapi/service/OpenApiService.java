f5f7e7582d70f52e945c8779b72dfb9f7aedc8a3
jshjshjshjsh <cj456456@gmail.com>
2023-05-15 16:37:21 +0900

    #126-feat/대기질 정보 api 요청

---
+++ src/main/java/com/example/demo/openapi/service/OpenApiService.java
@@ -0,0 +1,44 @@
+package com.example.demo.openapi.service;
+
+import com.example.demo.openapi.model.AirQualityDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+@Service
+public class OpenApiService {
+    public ArrayList<AirQualityDTO> convertToAirQuality(ResponseEntity<String> response) throws JsonProcessingException {
+
+
+        /* ObjectMapping from response */
+        ObjectMapper mapper = new ObjectMapper();
+
+        // todo: 여기 재활용할수있을 꺼 같은데
+        // note: readTree() 까지를 인터페이스랑 함수로 빼고 그 다음을 구현하는 방식?
+        // "response", "body"를 다중인자로 받고 for 문을 돌리고 제네릭쓰면 다 재활용 될듯
+
+
+        JsonNode result = mapper.readTree(response.getBody()).get("response").get("body").get("items");
+        ArrayList<AirQualityDTO> airKoreaDtos = new ArrayList<>();
+
+        for (Iterator<JsonNode> it = result.elements(); it.hasNext(); ) {
+            JsonNode node = it.next();
+            airKoreaDtos.add(mapper.treeToValue(node, AirQualityDTO.class));
+        }
+
+        return airKoreaDtos;
+    }
+    @ExceptionHandler(JsonProcessingException.class)
+    private void illegalArgumentExceptionHandler(HttpServletResponse response)
+    {
+        response.setStatus(HttpStatus.CONFLICT.value());
+    }
+}
