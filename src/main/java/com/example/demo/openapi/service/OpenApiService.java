843ee7eb01fa757384db248d45c3ffcfacc03fb2
jshjshjshjsh <cj456456@gmail.com>
2023-05-15 16:39:32 +0900

    #126-feat/open api 추가 확장 가능성 테스트를 위한 해양수질 요청 추가

--- src/main/java/com/example/demo/openapi/service/OpenApiService.java
+++ src/main/java/com/example/demo/openapi/service/OpenApiService.java
@@ -1,6 +1,7 @@
 package com.example.demo.openapi.service;
 
 import com.example.demo.openapi.model.AirQualityDTO;
+import com.example.demo.openapi.model.OceanQualityDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -36,6 +37,25 @@ public class OpenApiService {
 
         return airKoreaDtos;
     }
+    public ArrayList<OceanQualityDTO> convertToOceanQuality(ResponseEntity<String> response) throws JsonProcessingException {
+
+
+        /* ObjectMapping from response */
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        JsonNode result = mapper.readTree(response.getBody()).get("getWaterMeasuringListMavg").get("item");
+        ArrayList<OceanQualityDTO> oceanQualityDTOS = new ArrayList<>();
+
+        for (Iterator<JsonNode> it = result.elements(); it.hasNext(); ) {
+            JsonNode node = it.next();
+            oceanQualityDTOS.add(mapper.treeToValue(node, OceanQualityDTO.class).setupStationName());
+        }
+        System.out.println("oceanQualityDTOS = " + oceanQualityDTOS);
+
+        return oceanQualityDTOS;
+    }
+
     @ExceptionHandler(JsonProcessingException.class)
     private void illegalArgumentExceptionHandler(HttpServletResponse response)
     {
