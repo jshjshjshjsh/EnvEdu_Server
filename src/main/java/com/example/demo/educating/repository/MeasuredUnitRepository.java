41882d3e3f3ff27c219388d8ba18a8f6b8b209b4
jshjshjshjsh <cj456456@gmail.com>
2023-07-13 12:25:40 +0900

    #146-feat/Educating Repository 클래스 명 변경

---
+++ src/main/java/com/example/demo/educating/repository/MeasuredUnitRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.educating.repository;
+
+import com.example.demo.educating.model.MeasuredUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeasuredUnitRepository extends JpaRepository<MeasuredUnit, Long> {
+}
