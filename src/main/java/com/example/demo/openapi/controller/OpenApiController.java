636cd04dec8444f2857bf61ff05d28d282be5f01
jshjshjshjsh <cj456456@gmail.com>
2023-07-13 20:00:18 +0900

    #146-feat/공공데이터 라벨 변경 후 추가

--- src/main/java/com/example/demo/openapi/controller/OpenApiController.java
+++ src/main/java/com/example/demo/openapi/controller/OpenApiController.java
@@ -3,23 +3,21 @@ package com.example.demo.openapi.controller;
 import com.example.demo.jwt.util.JwtUtil;
 import com.example.demo.openapi.dto.AirQualityDTO;
 import com.example.demo.openapi.dto.OceanQualityDTO;
-import com.example.demo.openapi.dto.OpenApiParam;
+import com.example.demo.openapi.model.label.AirQualityLabels;
 import com.example.demo.openapi.model.entity.AirQuality;
 import com.example.demo.openapi.model.entity.OceanQuality;
-import com.example.demo.openapi.module.OpenApiRequest;
+import com.example.demo.openapi.model.label.OceanQualityLabels;
 import com.example.demo.openapi.service.OpenApiService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -32,22 +30,46 @@ public class OpenApiController {
     private final OpenApiService openApiService;
 
     @GetMapping("/air-quality")
-    public ResponseEntity<?> getAirQuality(@RequestParam(name="location", defaultValue = "부산") String location) throws UnsupportedEncodingException, JsonProcessingException {
+    public ResponseEntity<List<ResponseEntity<?>>>  getAirQuality(@RequestParam(name="location", defaultValue = "부산") String location) throws UnsupportedEncodingException, JsonProcessingException {
 
         String[] key = {"serviceKey", "returnType", "numOfRows", "pageNo", "sidoName", "ver"};
         String[] value = {serviceKey, "json", "100", "1", location, "1.0"};
 
         AirQualityDTO airQualityDTO = new AirQualityDTO();
-        return new ResponseEntity<>(airQualityDTO.convertToAirQuality(openApiService.callApi("https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?", key, value)), HttpStatus.OK);
+
+        AirQualityLabels airQualityLabels = new AirQualityLabels();
+        List<ResponseEntity<?>> responseEntities = new ArrayList<>();
+
+        ResponseEntity<?> labels = ResponseEntity.ok().body(airQualityLabels.getLabels());
+        ResponseEntity<?> datas = ResponseEntity.ok().body(airQualityDTO.convertToAirQuality(openApiService.callApi("https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?", key, value)));
+
+        responseEntities.add(labels);
+        responseEntities.add(datas);
+
+        return ResponseEntity.ok().body(responseEntities);
+        //return new ResponseEntity<>(airQualityDTOS, HttpStatus.OK);
     }
 
     @GetMapping("/ocean-quality")
     public ResponseEntity<?> getOceanQuality(@RequestParam(name="year", defaultValue = "2022") String wmyrList, @RequestParam(name="months", defaultValue = "06") String months) throws UnsupportedEncodingException, JsonProcessingException {
         String[] key = {"ServiceKey", "pageNo", "numOfRows", "resultType", "ptNoList", "wmyrList", "wmodList"};
         String[] value = {serviceKey, "1", "50", "JSON", "", wmyrList, months};
+
         ResponseEntity<String> stringResponseEntity = openApiService.callApi("https://apis.data.go.kr/1480523/WaterQualityService/getWaterMeasuringListMavg?", key, value);
         OceanQualityDTO oceanQualityDTO = new OceanQualityDTO();
-        return new ResponseEntity<>(oceanQualityDTO.convertToOceanQuality(stringResponseEntity), HttpStatus.OK);
+
+        OceanQualityLabels oceanQualityLabels = new OceanQualityLabels();
+        List<ResponseEntity<?>> responseEntities = new ArrayList<>();
+
+        ResponseEntity<?> labels = ResponseEntity.ok().body(oceanQualityLabels.getLabels());
+        ResponseEntity<?> datas = ResponseEntity.ok().body(oceanQualityDTO.convertToOceanQuality(stringResponseEntity));
+
+        responseEntities.add(labels);
+        responseEntities.add(datas);
+
+        return ResponseEntity.ok().body(responseEntities);
+
+        //return new ResponseEntity<>(oceanQualityDTO.convertToOceanQuality(stringResponseEntity), HttpStatus.OK);
     }
 
     @PostMapping("/air-quality")
--- src/main/java/com/example/demo/openapi/dto/OceanQualityDTO.java
+++ src/main/java/com/example/demo/openapi/dto/OceanQualityDTO.java
@@ -15,6 +15,7 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OceanQualityDTO extends OceanQualityParent {
 
+    public static String[] values = {"조사지점명","조사지점명","측정년도","측정월","측정값(수온) (단위 : °C)","측정값(수소이온 농도 (pH))","측정값(용존산소(DO)) (단위 : ㎎/L)","측정값(생물화학 적산소요구량(BOD)) (단위 : ㎎/L)","측정값(화학적산 소요구량(COD)) (단위 : ㎎/L)","측정값(총질소(T-N)) (단위 : ㎎/L)", "측정값(총인(T-P)) (단위 : ㎎/L)", "측정값(투명도) (단위 : ㎎/L)","측정값(클로로필-a(Chlorophyll-a)) (단위 : ㎎/L)", "측정값(전기전도도(EC)) (단위 : μS/㎝)", "측정값(총유기탄소(TOC)) (단위 : ㎎/L)"};
     public OceanQualityDTO setupStation() {
         setupStationName();
         return this;
