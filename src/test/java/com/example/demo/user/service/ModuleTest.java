794d317f32705298ea2349b086e7405e7a162173
jshjshjshjsh <cj456456@gmail.com>
2023-09-14 09:20:21 +0900

    rm/ModelTest 삭제, gitinore 추가

---
+++ src/test/java/com/example/demo/user/service/ModuleTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.user.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ModuleTest {
+
+    @Test
+    public void test(){
+        // Get field names
+        //AirQualityLabels airQualityLabels = new AirQualityLabels();
+
+        //System.out.println("airQualityLabels = " + airQualityLabels);
+    }
+}
