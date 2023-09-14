4b4c078aebf5d266cf63199c9df179c7d48ba74a
jshjshjshjsh <cj456456@gmail.com>
2023-09-14 09:07:30 +0900

    Revert "#126-feat/직렬화 시 순환참조 방지를 위해 JsonIgnore 속성 추가"
    This reverts commit edd2b0f8367b7ce7536c2925d773db6c98e50d97.

--- src/main/java/com/example/demo/openapi/model/entity/OceanQuality.java
+++ src/main/java/com/example/demo/openapi/model/entity/OceanQuality.java
@@ -2,7 +2,6 @@ package com.example.demo.openapi.model.entity;
 
 import com.example.demo.openapi.model.parent.OceanQualityParent;
 import com.example.demo.user.model.entity.User;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -15,7 +14,7 @@ public class OceanQuality extends OceanQualityParent {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
+    //@NotNull
     private User owner;
 
     public void setOwner(User owner) {
